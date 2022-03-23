3869524f91b1d7166be933427eb8377a2e05d25f
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-23 13:22:39 -0700

    finished choose_gesture methods for ai and player

--- ai.py
+++ ai.py
@@ -1,8 +1,10 @@
 from player import Player
+import random
 
 class AI(Player):
     def __init__(self):
         super().__init__()
 
     def choose_gesture(self):
-        pass
+        cho = random.choice(self.gestures)
+        self.gesture_choice = cho
--- player.py
+++ player.py
@@ -6,4 +6,15 @@ class Player:
 
     def choose_gesture(self):
         # input choice function
-        pass
+        while True:
+            for each in self.gestures:
+                print(each)
+            cho = input("Choose an option")
+            if cho in self.gestures:
+                self.gesture_choice = each
+                break
+            else:
+                print("Oops! Please type one of the options!")
+
+play = Player()
+play.choose_gesture()
